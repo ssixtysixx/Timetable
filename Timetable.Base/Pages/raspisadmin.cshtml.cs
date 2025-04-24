18052ebbe825391965b07fd327e110c5c3f2e5eb
sixty <nikimix06@gmail.com>
2025-04-24 19:25:15 +0300

    я сделал админ панель и всякие приколы через джпт

---
+++ Timetable.Base/Pages/raspisadmin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Timetable.Base.Pages
+{
+    public class raspisadminModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
