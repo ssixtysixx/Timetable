152f87d6bb182780889d52bc400461aa95335fdc
sixty <nikimix06@gmail.com>
2025-04-29 22:31:48 +0300

    привел все к MVC

---
+++ rasp/Controllers/raspisController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc;
+
+public class RaspisController : Controller
+{
+    public ActionResult Index()
+    {
+        // Создаем расписание
+        var schedule = new List<Raspisanie>
+        {
+            new Raspisanie { Id = 1, DayOfWeek = "Понедельник", Time = "09:00 - 10:30", Subject = "Математика", Teacher = "Иванов", Room = "101" },
+            new Raspisanie { Id = 2, DayOfWeek = "Понедельник", Time = "10:40 - 12:10", Subject = "Физика", Teacher = "Петров", Room = "202" },
+            new Raspisanie { Id = 3, DayOfWeek = "Вторник", Time = "09:00 - 10:30", Subject = "Программирование", Teacher = "Сидоров", Room = "305" },
+            new Raspisanie { Id = 4, DayOfWeek = "Среда", Time = "13:00 - 14:30", Subject = "Английский", Teacher = "Кузнецова", Room = "104" },
+            new Raspisanie { Id = 5, DayOfWeek = "Четверг", Time = "10:40 - 12:10", Subject = "История", Teacher = "Васильева", Room = "207" }
+        };
+
+        // Преобразуем в SelectListItem для ListBox
+        var listItems = schedule.Select(x => new SelectListItem
+        {
+            Text = $"{x.DayOfWeek} | {x.Time} | {x.Subject} | {x.Teacher} | Ауд. {x.Room}",
+            Value = x.Id.ToString()
+        }).ToList();
+
+        ViewBag.ScheduleItems = listItems;
+
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Index(List<int> SelectedItems)
+    {
+        // Обработка выбранных занятий (например, запись в БД)
+        if (SelectedItems != null)
+        {
+            foreach (var id in SelectedItems)
+            {
+                // Логика обработки выбранных элементов
+            }
+        }
+        return RedirectToAction("Index");
+    }
+}
---
+++ rasp/Controllers/raspisadminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace rasp.Controllers
+{
+    public class raspisadminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
