163529f7ba9de748352be2b4dc6641e9d216566c
Yaroslav <qwerty_qwerty12345678910@mail.ru>
2025-05-20 21:59:12 +0300

    add css file

--- rasp/Controllers/raspisController.cs
+++ rasp/Controllers/raspisController.cs
@@ -2,11 +2,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Timetable.Framework;
 using Timetable.Framework.Records;
+using System.Text.RegularExpressions;
 
 public class RaspisController : Controller
 {
 	private readonly IDisciplineRepository _Disciplinerepository;
-	private readonly IGroupRepository _Grouprepository;
+	private readonly IGroupRepository _grouprepository;
 	private readonly IPlaceRepository _Placerepository;
 	private readonly IRecordRepository _Recordrepository;
 	private readonly ITeacherRepository _Teacherrepository;
@@ -17,34 +18,17 @@ public class RaspisController : Controller
 	public RaspisController(IDisciplineRepository Disciplinerepository, IGroupRepository Grouprepository, IPlaceRepository Placerepository, IRecordRepository Recordrepository, ITeacherRepository Teacherrepository)
 	{
 		_Disciplinerepository = Disciplinerepository;
-		_Grouprepository = Grouprepository;
+		_grouprepository = Grouprepository;
 		_Placerepository = Placerepository;
 		_Recordrepository = Recordrepository;
 		_Teacherrepository = Teacherrepository;
 	}
 
-	public ActionResult Index()
+    [HttpGet]
+	public async Task<ActionResult> Index()
 	{
-		var schedule = new List<Raspisanie>
-		{
-			new Raspisanie { Id = 1, DayOfWeek = "Понедельник", Time = "09:00 - 10:30", Subject = "Математика", Teacher = "Иванов", Room = "101" },
-			new Raspisanie { Id = 2, DayOfWeek = "Понедельник", Time = "10:40 - 12:10", Subject = "Физика", Teacher = "Петров", Room = "202" },
-			new Raspisanie { Id = 3, DayOfWeek = "Вторник", Time = "09:00 - 10:30", Subject = "Программирование", Teacher = "Сидоров", Room = "305" },
-			new Raspisanie { Id = 4, DayOfWeek = "Среда", Time = "13:00 - 14:30", Subject = "Английский", Teacher = "Кузнецова", Room = "104" },
-			new Raspisanie { Id = 5, DayOfWeek = "Четверг", Time = "10:40 - 12:10", Subject = "История", Teacher = "Васильева", Room = "207" }
-		};
-
-		Task.Run(ActivateAsync);
-
-		var listItems = schedule.Select(x => new SelectListItem
-		{
-			Text = $"{x.DayOfWeek} | {x.Time} | {x.Subject} | {x.Teacher} | Ауд. {x.Room}",
-			Value = x.Id.ToString()
-		}).ToList();
-
-		ViewBag.ScheduleItems = listItems;
-
-		return View();
+		await ActivateAsync();
+		return View(Records);
 	}
 
 	public async Task<ActionResult> ActivateAsync()
@@ -94,23 +78,9 @@ public class RaspisController : Controller
 		}; 
 
 		var groupList = new List<GroupDayRecord>() { groupDayRecord };
-
+        
 		Records = groupList;
 
 		return View(groupList);
-	}
-
-	[HttpPost]
-	public ActionResult Index(List<int> SelectedItems)
-	{
-
-		if (SelectedItems != null)
-		{
-			foreach (var id in SelectedItems)
-			{
-				
-			}
-		}
-		return RedirectToAction("Index");
 	}
 }
